d936aa29c4c1b388799a32b1a3e3e38f3680b451
James Muirhead <60221059+JamesMuirhead@users.noreply.github.com>
2021-03-16 12:57:00 +0000

    Buttons and slider generate ints

---
+++ Puzzle Prototype/Assets/ButtonPress.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPress : MonoBehaviour
+{
+    private Button button;
+    private Text buttonLabel;
+    private int buttonNum;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = gameObject.GetComponent<Button>();
+        buttonLabel = gameObject.GetComponentInChildren<Text>();
+        buttonLabel.text = gameObject.name;
+        buttonNum = Convert.ToInt32(gameObject.name);
+    }
+
+    public void ButtonPressed()
+    {
+        Debug.Log("Button " + buttonNum + " was pressed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Puzzle Prototype/Assets/SlidePos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlidePos : MonoBehaviour
+{
+    private Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = gameObject.GetComponent<Slider>();
+        Debug.Log("Slider is at " + slider.value);
+    }
+
+    public void SliderChange()
+    {
+        Debug.Log("Slider is at " + slider.value);
+    }
+}
