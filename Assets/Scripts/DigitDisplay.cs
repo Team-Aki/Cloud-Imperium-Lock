b5ff3a6dc35b4bed6320b27bd871d6a1b6e14c22
Gianluca Pumoni <1603589+LucasTheAlchemist@users.noreply.github.com>
2021-03-17 13:49:38 +0000

    Implemented Difficulty
    Easy: 4 digits
    Normal: 5 digits
    Hard: 6 digits

--- Assets/Scripts/DigitDisplay.cs
+++ Assets/Scripts/DigitDisplay.cs
@@ -5,105 +5,142 @@ using UnityEngine.UI;
 
 public class DigitDisplay : MonoBehaviour
 {
-    [SerializeField] private Sprite[] digits;
-    [SerializeField] private Image[] characters;
+    public enum Difficulty
+    {
+        EASY,
+        NORMAL,
+        HARD
+    }
 
-    private string codeSequence;
+    /*[SerializeField] private Sprite[] digits;
+    [SerializeField] private Image[] characters;*/
+    private Text displayCode;
+    [SerializeField] private Difficulty difficulty;
+
+    //private string codeSequence;
 
     private void Start()
     {
-        codeSequence = "";
+        displayCode = gameObject.GetComponentInChildren<Text>();
 
-        for (int i = 0; i < characters.Length; i++)
+        displayCode.text = "";
+
+        //codeSequence = "";
+
+        /*for (int i = 0; i < characters.Length; i++)
         {
             characters[i].sprite = digits[10];
-        }
+        }*/
 
         PressButton.ButtonPressed += AddDigitToCode;
     }
 
     private void AddDigitToCode(string digitEntered)
     {
-        if (codeSequence.Length < 4)
+        switch (difficulty)
         {
-            switch (digitEntered)
-            {
-                case "Zero":
-                    codeSequence += 0;
-                    DisplayCodeSequence(0);
-                    break;
-                case "One":
-                    codeSequence += 1;
-                    DisplayCodeSequence(1);
-                    break;
-                case "Two":
-                    codeSequence += 2;
-                    DisplayCodeSequence(2);
-                    break;
-                case "Three":
-                    codeSequence += 3;
-                    DisplayCodeSequence(3);
-                    break;
-                case "Four":
-                    codeSequence += 4;
-                    DisplayCodeSequence(4);
-                    break;
-                case "Five":
-                    codeSequence += 5;
-                    DisplayCodeSequence(5);
-                    break;
-                case "Six":
-                    codeSequence += 6;
-                    DisplayCodeSequence(6);
-                    break;
-                case "Seven":
-                    codeSequence += 7;
-                    DisplayCodeSequence(7);
-                    break;
-                case "Eight":
-                    codeSequence += 8;
-                    DisplayCodeSequence(8);
-                    break;
-                case "Nine":
-                    codeSequence += 9;
-                    DisplayCodeSequence(9);
-                    break;
-            }
+            case Difficulty.EASY:
+                if (displayCode.text.Length < 4)
+                {
+                    EnterCode(digitEntered);
+                }
+                break;
+            case Difficulty.NORMAL:
+                if (displayCode.text.Length < 5)
+                {
+                    EnterCode(digitEntered);
+                }
+                break;
+            case Difficulty.HARD:
+                if (displayCode.text.Length < 6)
+                {
+                    EnterCode(digitEntered);
+                }
+                break;
         }
+        
 
         //Reset/cancel last character
     }
 
-    private void DisplayCodeSequence(int digit)
+    private void EnterCode(string digitEntered)
     {
-        switch (codeSequence.Length)
+        switch (digitEntered)
         {
-            case 1:
-                characters[0].sprite = digits[10];
-                characters[1].sprite = digits[10];
-                characters[2].sprite = digits[10];
-                characters[3].sprite = digits[digit];
+            case "Zero":
+                displayCode.text += 0;
+                //DisplayCodeSequence(0);
                 break;
-            case 2:
-                characters[0].sprite = digits[10];
-                characters[1].sprite = digits[10];
-                characters[2].sprite = characters[3].sprite;
-                characters[3].sprite = digits[digit];
+            case "One":
+                displayCode.text += 1;
+                //DisplayCodeSequence(1);
                 break;
-            case 3:
-                characters[0].sprite = digits[10];
-                characters[1].sprite = characters[2].sprite;
-                characters[2].sprite = characters[3].sprite;
-                characters[3].sprite = digits[digit];
+            case "Two":
+                displayCode.text += 2;
+                //DisplayCodeSequence(2);
                 break;
-            case 4:
-                characters[0].sprite = characters[1].sprite;
-                characters[1].sprite = characters[2].sprite;
-                characters[2].sprite = characters[3].sprite;
-                characters[3].sprite = digits[digit];
+            case "Three":
+                displayCode.text += 3;
+                //DisplayCodeSequence(3);
+                break;
+            case "Four":
+                displayCode.text += 4;
+                //DisplayCodeSequence(4);
+                break;
+            case "Five":
+                displayCode.text += 5;
+                //DisplayCodeSequence(5);
+                break;
+            case "Six":
+                displayCode.text += 6;
+                //DisplayCodeSequence(6);
+                break;
+            case "Seven":
+                displayCode.text += 7;
+                //DisplayCodeSequence(7);
+                break;
+            case "Eight":
+                displayCode.text += 8;
+                //DisplayCodeSequence(8);
+                break;
+            case "Nine":
+                displayCode.text += 9;
+                //DisplayCodeSequence(9);
                 break;
         }
     }
+
+    /* private void DisplayCodeSequence(int digit)
+     {
+         switch (displayCode.text.Length)
+         {
+
+             *//*case 1:
+                 characters[0].sprite = digits[10];
+                 characters[1].sprite = digits[10];
+                 characters[2].sprite = digits[10];
+                 characters[3].sprite = digits[digit];
+                 break;
+             case 2:
+                 characters[0].sprite = digits[10];
+                 characters[1].sprite = digits[10];
+                 characters[2].sprite = characters[3].sprite;
+                 characters[3].sprite = digits[digit];
+                 break;
+             case 3:
+                 characters[0].sprite = digits[10];
+                 characters[1].sprite = characters[2].sprite;
+                 characters[2].sprite = characters[3].sprite;
+                 characters[3].sprite = digits[digit];
+                 break;
+             case 4:
+                 characters[0].sprite = characters[1].sprite;
+                 characters[1].sprite = characters[2].sprite;
+                 characters[2].sprite = characters[3].sprite;
+                 characters[3].sprite = digits[digit];
+                 break;*//*
+         }
+     }*/
 
     private void CheckResults()
     {
@@ -113,9 +150,9 @@ public class DigitDisplay : MonoBehaviour
 
     private void ResetDisplay()
     {
-        for (int i = 0; i < characters.Length; i++)
+        for (int i = 0; i < displayCode.text.Length; i++)
         {
-            characters[i].sprite = digits[10];
+            displayCode.text = "";
         }
     }
 
