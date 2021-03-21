a1fba4cea366d09dac1e54d89a6ba02020dfb189
Morgan Skillicorn <hello@morgan.games>
2021-03-21 02:51:21 +0000

    created most of the gameplay loop system and added HDRP

---
+++ Assets/Prefabs/DoorManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject lockObject;
+    [SerializeField]
+    public GameObject doorObject;
+
+    public enum State { open, closed }
+    [SerializeField]
+    public State state = State.closed;
+
+    [SerializeField]
+    public bool codeExpired = false;
+
+    public enum Difficulty { easy, medium, hard, wtf }
+    [SerializeField]
+    public Difficulty difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetLock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (state == State.closed)
+        //{
+        //    if (!lockObject.GetComponent</*LOCK SCRIPT*/>().locked)
+        //    {
+        //        doorObject.GetComponent<Animator>().SetBool("open", true);
+        //    }
+        //    else
+        //    {
+        //        doorObject.GetComponent<Animator>().SetBool("open", false);
+        //    }
+        //}
+        //else if(state == State.open)
+        //{
+        //    doorObject.GetComponent<Animator>().SetBool("open", true);
+        //}
+    }
+
+    public void ResetLock()
+    {
+        //create a new lock based on the difficulty set above as a child of lockObject;
+
+        state = State.closed;
+        codeExpired = false;
+    }
+}
---
+++ Assets/Prefabs/LoopManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainCam;
+
+    [SerializeField]
+    private GameObject[] doors = new GameObject[4];
+
+    [SerializeField]
+    private Transform[] cameraPlacements = new Transform[4];
+
+    [SerializeField]
+    private int activeDoor = 0;
+
+    private bool doorTriggeredLastFrame = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doors[activeDoor].GetComponent<DoorManager>().state == DoorManager.State.closed)
+        {
+            doorTriggeredLastFrame = false;
+        }
+        else if (doors[activeDoor].GetComponent<DoorManager>().state == DoorManager.State.open)
+        {
+            if (!doors[activeDoor].GetComponent<DoorManager>().doorObject.GetComponent<Animation>().isPlaying && !doorTriggeredLastFrame)
+            {
+                doors[activeDoor].GetComponent<DoorManager>().doorObject.GetComponent<Animation>().clip = doors[activeDoor].GetComponent<DoorManager>().doorObject.GetComponent<Animation>().GetClip("Open");
+                doors[activeDoor].GetComponent<DoorManager>().doorObject.GetComponent<Animation>().Play();
+            }
+            else if (!doors[activeDoor].GetComponent<DoorManager>().doorObject.GetComponent<Animation>().isPlaying)
+            {
+                doors[activeDoor].GetComponent<DoorManager>().ResetLock();
+                activeDoor++;
+            }
+
+            doorTriggeredLastFrame = true;
+        }
+        
+        if (doors[activeDoor].GetComponent<DoorManager>().codeExpired)
+        {
+            doors[activeDoor].GetComponent<DoorManager>().ResetLock();
+            activeDoor--;
+        }
+
+        if (activeDoor < 0)
+            activeDoor = 0;
+    }
+
+    void LateUpdate()
+    {
+        mainCam.transform.position = Vector3.MoveTowards(mainCam.transform.position, cameraPlacements[activeDoor].position, 0.2f);
+        mainCam.transform.rotation = cameraPlacements[activeDoor].rotation;
+    }
+}
